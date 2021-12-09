b9f16bb7758fc1571d937d8bb8fcd793aedf25fb
Alex Marmolejo <alex97marmolejo@gmail.com>
2021-12-08 23:19:19 -0500

    added submission and editing logic. added input handler

--- src/App.js
+++ src/App.js
@@ -3,44 +3,68 @@ import './styles/App.css';
 import General from './components/General';
 import Education from './components/Education';
 import Experience from './components/Experience';
+import Application from './components/Application';
 
 class App extends Component {
   constructor(props) {
     super(props)
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputs = this.handleInputs.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
 
     this.state = {
-      personalInfo: {
         name: '',
         email: '',
         phone: '',
-      },
-      educationInfo: {
         school: '',
         major: '',
-        date: '',
-      },
-      experienceInfo: {
+        date: new Date().toLocaleDateString('en-US'),
         company: '',
         position: '',
         jobDescription: '',
-        dateFrom: new Date(),
-        dateTo: new Date(),
-      }
+        dateFrom: new Date().toLocaleDateString('en-US'),
+        dateTo: new Date().toLocaleDateString('en-US'),
+        isSubmitted: false,
     }
   }
 
+  handleInputs(event) {
+    this.setState({
+      ...this.state,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  handleSubmit() {
+    console.log(this.state);
+    this.setState({
+      isSubmitted: true,
+    })
+  }
+
+  handleEdit() {
+    this.setState({
+      isSubmitted: false,
+    })
+  }
+
   render() {
-    return (
-      <div>
-        <form>
-          <General header='general info' />
-          <Education header='education' />
-          <Experience header='experience' />
-          <button type='button'>edit</button>
-          <button type='button'>start</button>
-        </form>
-      </div>
-    );
+    if(this.state.isSubmitted) {
+      return (
+        <Application handleEdit={this.handleEdit} applicationInfo={this.state} />
+      );
+    } else {
+      return (
+        <div>
+          <form>
+            <General header='general info' handleInputs={this.handleInputs} />
+            <Education header='education' handleInputs={this.handleInputs} />
+            <Experience header='experience' handleInputs={this.handleInputs} />
+            <button type='button' onClick={this.handleSubmit}>submit</button>
+          </form>
+        </div>
+      );
+    }
   }
 }
 
