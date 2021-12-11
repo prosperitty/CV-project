4f894592f726f9362fe7be675df9fe60cd7ba419
Alex Marmolejo <alex97marmolejo@gmail.com>
2021-12-10 20:19:59 -0500

    fixed bug where textarea input did not have event handler

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -14,7 +14,7 @@ export default class Experience extends Component {
         <Input inputType='text' inputName='position' info={this.props.applicationInfo.position} handleInputs={this.props.handleInputs} />
         <div className='input-container'>
           <label htmlFor='jobDescription'>job description</label>
-          <textarea name='jobDescription' info={this.props.applicationInfo.jobDescription} handleInputs={this.props.handleInputs} />
+          <textarea name='jobDescription' value={this.props.applicationInfo.jobDescription} onChange={this.props.handleInputs} />
         </div>
         <Input inputType='date' inputName='dateFrom' info={this.props.applicationInfo.dateFrom} handleInputs={this.props.handleInputs} />
         <Input inputType='date' inputName='dateTo' info={this.props.applicationInfo.dateTo} handleInputs={this.props.handleInputs} />
