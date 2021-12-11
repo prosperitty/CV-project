47c236a3ceee28046ca39a67b641182dc7986489
Alex Marmolejo <alex97marmolejo@gmail.com>
2021-12-10 20:20:20 -0500

    initialized application structure

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,23 +7,34 @@ export default class Application extends Component {
  
   render() {
     return (
-      <div>
-        <div>
-          <p>{this.props.applicationInfo.name}</p>
-          <p>{this.props.applicationInfo.email}</p>
-          <p>{this.props.applicationInfo.phone}</p>
+      <div className='application'>
+        <div className='application-personal'>
+          <h1 className='info-name'>{this.props.applicationInfo.name}</h1>
+          <p className='container-email-phone'>
+            <span className='info-email'>{this.props.applicationInfo.email}</span>
+            <span className='info-phone'>{this.props.applicationInfo.phone}</span>
+          </p>
         </div>
-        <div>
-          <p>{this.props.applicationInfo.school}</p>
-          <p>{this.props.applicationInfo.major}</p>
-          <p>{this.props.applicationInfo.date}</p>
+        <h4 className='application-title'>education</h4>
+        <div className='application-education'>
+          <h4 className='info-school'>{this.props.applicationInfo.school}</h4>
+          <div className='container-major-date'>
+            <span className='info-major'>{this.props.applicationInfo.major}</span>
+            <span className='info-school-date'>{this.props.applicationInfo.date}</span>
+          </div>
         </div>
-        <div>
-          <p>{this.props.applicationInfo.company}</p>
-          <p>{this.props.applicationInfo.position}</p>
-          <p>{this.props.applicationInfo.jobDescription}</p>
-          <p>{this.props.applicationInfo.dateFrom}</p>
-          <p>{this.props.applicationInfo.dateTo}</p>
+        <h4 className='application-title'>experience</h4>
+        <div className='application-experience'>
+          <h4 className='info-company'>{this.props.applicationInfo.company}</h4>
+          <div className='container-position-date'>
+            <span className='info-position'>{this.props.applicationInfo.position}</span>
+            <div>
+              <span className='info-date-from'>{this.props.applicationInfo.dateFrom}</span>
+              <span> - </span>
+              <span className='info-date-to'>{this.props.applicationInfo.dateTo}</span>
+            </div>
+          </div>
+          <p className='info-job-description'>{this.props.applicationInfo.jobDescription}</p>
         </div>
         <button type='button' onClick={this.props.handleEdit}>edit</button>
       </div>
